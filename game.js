42a5a9aae800d629ea0093b1d9965694eb0689be
Luka France <lukafrance@gmail.com>
2022-05-11 15:38:56 +0100

    first commit

--- game.js
+++ game.js
@@ -14,62 +14,61 @@ const petBoxSingular = document.getElementById("petBoxSingular");
 const buttonRevivePet = document.getElementById("buttonRevivePet");
 
 //petIcon//
-document.getElementById("petIcon").src="cyberPetCatNeutral.png";
+document.getElementById("petIcon").src="porygon.png";
 
 
 //Interacting with Pet//
 buttonPlayWith.addEventListener("click", ()=>{ //increases value of play level when clicked//
     objectLvlPlay.value+=20;
 })
-buttonPlayWith.addEventListener("mousedown", ()=>{ //makes cat happy when interacted with//
-    petIcon.src="cyberPetCatHappy.png";
-})
+
 buttonPlayWith.addEventListener("mouseup", ()=>{ //returns cat to neutral state on mouse release//
-    petIcon.src="cyberPetCatNeutral.png";
+    petIcon.src="porygon.png";
 })
 buttonFeed.addEventListener("click", ()=>{ //increases value of hunger level when clicked//
     objectLvlHunger.value+=20;
 })
 buttonFeed.addEventListener("mousedown", ()=>{
-    petIcon.src="cyberPetCatHappy.png";
+    petIcon.src="porygon.png";
 })
 buttonFeed.addEventListener("mouseup", ()=>{
-    petIcon.src="cyberPetCatNeutral.png";
+    petIcon.src="porygon.png";
 })
 buttonWater.addEventListener("click", ()=>{//increases value of thirst level when clicked//
     objectLvlThirst.value+=20;
 })
 buttonWater.addEventListener("mousedown", ()=>{
-    petIcon.src="cyberPetCatHappy.png";
+    petIcon.src="porygon.png";
 })
 buttonWater.addEventListener("mouseup", ()=>{
-    petIcon.src="cyberPetCatNeutral.png";
+    petIcon.src="porygon.png";
 })
 buttonWalk.addEventListener("click", ()=>{//increases value of walks level when clicked//
     objectLvlWalks.value+=20;
 })
 buttonWalk.addEventListener("mousedown", ()=>{
-    petIcon.src="cyberPetCatHappy.png";
+    petIcon.src="porygon.png";
 })
 buttonWalk.addEventListener("mouseup", ()=>{
-    petIcon.src="cyberPetCatNeutral.png";
+    petIcon.src="porygon.png";
 })
 buttonKillPet.addEventListener("click", () => { //to make pet dead//
     objectLvlPlay.value=0;
     objectLvlHunger.value=0;
     objectLvlThirst.value=0;
     objectLvlWalks.value=0;
-    petIcon.src="cyberPetCatDead.png";
+    petIcon.src="porygon.png";
     petBoxSingular.style.background = "grey";
     buttonKillPet.style.display = "none";
     buttonRevivePet.style.display = "block";
+    alert("you are gonna regret that!")
 })
 buttonRevivePet.addEventListener("click", () => { //to revive cat//
     objectLvlPlay.value=1000;
     objectLvlHunger.value=100;
     objectLvlThirst.value=100;
     objectLvlWalks.value=100;
-    petIcon.src="cyberPetCatNeutral.png";
+    petIcon.src="porygon.png";
     petBoxSingular.style.background = " #F4CEC7";
     buttonKillPet.style.display = "block";
     buttonRevivePet.style.display = "none";
@@ -82,26 +81,15 @@ objectLvlWalks.value = 100;
 
 let play = setInterval(() => {
     objectLvlPlay.value--;
-}, 1000); 
+}, 200); 
 let hunger = setInterval(() => {
     objectLvlHunger.value--;
-}, 1100); 
+}, 100); 
 let thirst = setInterval(() => {
     objectLvlThirst.value--;
 }, 1200); 
 let walks = setInterval(() => {
     objectLvlWalks.value--;
-}, 2000); 
+}, 1800); 
 
-if(objectLvlPlay.value == 0) {
-    clearInterval(play);
-}
-if(objectLvlHunger.value == 0) {
-    clearInterval(hunger);
-}
-if(objectLvlThirst.value == 0) {
-    clearInterval(thirst);
-}
-if(objectLvlWalks.value == 0) {
-    clearInterval(walks);
-}
+ 
